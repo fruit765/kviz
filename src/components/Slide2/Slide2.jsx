1d8407d86e92d012a89415bca30a49dbb4c68d25
Mihail <sidorov.mihail.maykop@mail.ru>
2021-06-10 18:23:29 +0300

    Сверстал второй слайд и сделал механизм сохранения результата ответа на вопрос.

--- src/components/Slide2/Slide2.jsx
+++ src/components/Slide2/Slide2.jsx
@@ -1,7 +1,81 @@
+import './Slide2.scss';
+import img1 from '../../imgs/slide2__wrapper-img1.svg';
+import img2 from '../../imgs/slide2__wrapper-img2.svg';
+import NextSlideBtn from '../NextSlideBtn/NextSlideBtn';
+import { useContext, useState } from 'react';
+import cl from 'classnames';
+import { SlidesContext } from '../Slides/Slides';
+
 export default function Slide2() {
+    const [question, changeQuestion] = useState({
+        title: 'Какой у вас питомец?',
+        type: 'inform',
+        answers: [
+            {
+                title: 'Кошка',
+                isPick: false,
+            },
+            {
+                title: 'Собака',
+                isPick: false,
+            },
+        ],
+    });
+
+    const dispatch = useContext(SlidesContext);
+
+    let answers = [];
+    question.answers.forEach((el, index) => {
+        answers.push(
+            <button className={cl('slide2__wrapper-answer', el.isPick && 'slide2__wrapper-answer_is-pick')} key={index}
+                onClick={() => {
+                    let q = {...question};
+                    for (let answer of q.answers) {
+                        answer.isPick = false;
+                    }
+                    q.answers[index].isPick = true;
+                    changeQuestion(q);
+                }}
+            >
+                {el.title}
+            </button>
+        );
+    });
+
     return (
         <div className="slide2">
-            Слайд2
+            <div className="slide2__front-background">
+                <div className="slide2__wrapper">
+                    <div className="slide2__wrapper-title">
+                        Вопрос 1:<br />
+                        {question.title}
+                    </div>
+                    <div className="slide2__wrapper-imgs">
+                        <div className="slide2__wrapper-img">
+                            <img src={img1} />
+                        </div>
+                        <div className="slide2__wrapper-img">
+                            <img src={img2} />
+                        </div>
+                        <div className="slide2__wrapper-answers">
+                            {answers}
+                        </div>
+                    </div>
+                    <div className="slide2__wrapper-btn">
+                        <NextSlideBtn text="Далее"
+                            callback={() => {
+                                dispatch({
+                                    type: 'ADD_QUESTION',
+                                    question: question,
+                                });
+                                dispatch({
+                                    type: 'CHANGE_SLIDE',
+                                });
+                            }}
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/Slides/Slides.jsx
+++ src/components/Slides/Slides.jsx
@@ -1,15 +1,25 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 
 import './Slides.scss';
 
 import Slide1 from "../Slide1/Slide1";
 import Slide2 from "../Slide2/Slide2";
+import Slide3 from "../Slide3/Slide3";
 
 function reducer(state, action) {
     switch (action.type) {
         case 'CHANGE_SLIDE':
+            // let newState = {...state};
+            // newState.slideNum = newState.slideNum + 1;
+            // console.log(newState);
+            // return newState;
+
             state.slideNum = state.slideNum + 1;
+            console.log({...state});
             return {...state};
+        case 'ADD_QUESTION':
+            state.questions.push(action.question);
+            return state;
         default:
             return state;
     }
@@ -18,11 +28,16 @@ function reducer(state, action) {
 export const SlidesContext = React.createContext();
 
 export default function Slides() {
+    
     const initialState = {
         slideNum: 1,
         questions: [],
     };
     const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(() => {
+        console.log("useEffect slideNum >>>", state.slideNum);
+    });
     
     let slide;
     switch (state.slideNum) {
@@ -31,6 +46,9 @@ export default function Slides() {
             break;
         case 2:
             slide = <Slide2 />
+            break;
+        case 3:
+            slide = <Slide3 />
             break;
         default:
             slide = null;
